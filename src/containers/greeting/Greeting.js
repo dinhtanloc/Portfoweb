a0c8d5f145992d779b91bcbfc8f4b7a624d8ba58
Dinh Tan Loc <dinhtanloc.dynamicueh@gmail.com>
2025-07-04 03:59:26 +0700

    Feat: update CV

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,7 +42,7 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
+                    href={require("./Dinh_Tan_Loc_resume_AI.pdf")}
                     download="Resume.pdf"
                     className="download-link-button"
                   >
