aa53a860e495d4c4b54cb50a51f325dab0467a68
Dinh Tan Loc <dinhtanloc.dynamicueh@gmail.com>
2025-07-25 00:50:58 +0700

    merge

--- src/portfolio.js
+++ src/portfolio.js
@@ -261,7 +261,7 @@ const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
     {
-      role: "Junior AI Engineer",
+      role: "AI Engineer",
       company: "Hitachi Digital Services",
       companylogo: require("./assets/images/hitachi_logo.png"),
       date: "July 2025 – Present",
